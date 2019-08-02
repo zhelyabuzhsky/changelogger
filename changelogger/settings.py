79443f1057b1fe4e887ff6a1420442a2da220d9c
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-08-02 15:53:15 +0500

    Added app changelogs

--- changelogger/settings.py
+++ changelogger/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'changelogs.apps.ChangelogsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
