4be1d4d9188a1ddbc392170cc83390d42b1bee56
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-08-02 16:30:39 +0500

    Added test for index page

--- changelogs/tests.py
+++ changelogs/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ChangelogsIndexViewTests(TestCase):
+    def test_success(self):
+        response = self.client.get(reverse('changelogs:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Collector for you changelogs")
--- changelogs/urls.py
+++ changelogs/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'changelogs'
+
 urlpatterns = [
     path('', views.index, name='index'),
 ]
