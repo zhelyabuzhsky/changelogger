4400b4d2116f6962d8f2df18f42510bb0c5e730c
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-08-09 14:32:43 +0500

    Fixed typo test

--- changelogs/tests.py
+++ changelogs/tests.py
@@ -6,4 +6,4 @@ class ChangelogsIndexViewTests(TestCase):
     def test_success(self):
         response = self.client.get(reverse("changelogs:index"))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Collector for you changelogs")
+        self.assertContains(response, "Collector for your changelogs")
