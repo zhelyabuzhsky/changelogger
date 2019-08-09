7c3b7df4ef4966d66d87105029ee59a56e066700
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-08-09 16:06:34 +0500

    Added subscribers attribute to Project model

--- changelogs/models.py
+++ changelogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Project(models.Model):
@@ -7,6 +8,7 @@ class Project(models.Model):
 
     title = models.CharField(max_length=200)
     url = models.URLField(max_length=200)
+    subscribers = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return f"{self.title} ({self.url})"
