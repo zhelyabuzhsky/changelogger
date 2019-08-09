96072959e748c84680ca01ccd6bbbc54e5cd3bd2
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-08-09 14:14:59 +0500

    Added Project model

--- changelogs/models.py
+++ changelogs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    class Meta:
+        db_table = 'projects'
+
+    title = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return f"{self.title} ({self.url})"
