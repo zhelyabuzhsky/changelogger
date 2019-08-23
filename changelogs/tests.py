f992def94a245c29b5c994cb5b95d2747488ab3e
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-08-23 12:14:27 +0500

    Added page My projects (#8)

--- changelogs/tests.py
+++ changelogs/tests.py
@@ -1,7 +1,9 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
 
 from .models import Project
+from .views import my_projects
 
 
 class IndexViewTests(TestCase):
@@ -25,6 +27,8 @@ class AllProjectsViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "django")
         self.assertContains(response, "https://github.com/django/django")
+        self.assertContains(response, "requests")
+        self.assertContains(response, "https://github.com/psf/requests")
         self.assertQuerysetEqual(
             response.context["all_projects_list"],
             [
@@ -32,3 +36,32 @@ class AllProjectsViewTests(TestCase):
                 "<Project: requests (https://github.com/psf/requests)>",
             ],
         )
+
+
+class MyProjectsViewTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username="jacob", email="jacob@mail.com", password="top_secret"
+        )
+
+    def test_no_projects(self):
+        response = self.client.get(reverse("changelogs:all_projects"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No projects are available.")
+        self.assertQuerysetEqual(response.context["all_projects_list"], [])
+
+    def test_success(self):
+        project_django = Project.objects.create(
+            title="django", url="https://github.com/django/django"
+        )
+        project_django.subscribers.add(self.user)
+        Project.objects.create(title="requests", url="https://github.com/psf/requests")
+        request = self.factory.get(reverse("changelogs:my_projects"))
+        request.user = self.user
+        response = my_projects(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "django")
+        self.assertContains(response, "https://github.com/django/django")
+        self.assertNotContains(response, "requests")
+        self.assertNotContains(response, "https://github.com/psf/requests")
--- changelogs/urls.py
+++ changelogs/urls.py
@@ -11,6 +11,7 @@ router.register(r"projects", views.ProjectViewSet)
 urlpatterns = [
     path("", views.index, name="index"),
     path("projects/all/", views.all_projects, name="all_projects"),
+    path("projects/my/", views.my_projects, name="my_projects"),
     path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
