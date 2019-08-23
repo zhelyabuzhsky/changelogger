3a169651d7cc00621d2ad3fd224eb8ea029da1ba
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-08-23 16:02:00 +0500

    Added version detail page

--- changelogs/urls.py
+++ changelogs/urls.py
@@ -12,6 +12,11 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("projects/all/", views.all_projects, name="all_projects"),
     path("projects/my/", views.my_projects, name="my_projects"),
+    path(
+        "projects/<int:project_id>/versions/<int:version_id>/",
+        views.version_detail,
+        name="version_detail",
+    ),
     path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
