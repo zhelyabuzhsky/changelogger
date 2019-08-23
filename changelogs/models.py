975d0bffcda882ed0f09d663fdcdc240e32bfd09
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-08-23 15:21:06 +0500

    Added Version model

--- changelogs/models.py
+++ changelogs/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Project(models.Model):
@@ -12,3 +12,16 @@ class Project(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.url})"
+
+
+class Version(models.Model):
+    class Meta:
+        db_table = "versions"
+
+    title = models.CharField(max_length=20)
+    date_time = models.DateTimeField()
+    body = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} ({self.project.title})"
