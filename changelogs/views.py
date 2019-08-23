5db7a93d9a2eb390dce4709493fd2a0816a0c678
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-08-23 12:07:50 +0500

    Added page My projects

--- changelogs/views.py
+++ changelogs/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from rest_framework import viewsets
+
 from .models import Project
 from .serializers import ProjectSerializer
 
@@ -15,6 +16,13 @@ def all_projects(request):
     template = loader.get_template("changelogs/all_projects.html")
     all_projects_list = Project.objects.order_by("pk").all()
     context = {"all_projects_list": all_projects_list}
+    return HttpResponse(template.render(context, request))
+
+
+def my_projects(request):
+    template = loader.get_template("changelogs/my_projects.html")
+    my_projects_list = Project.objects.filter(subscribers=request.user).order_by("pk").all()
+    context = {"my_projects_list": my_projects_list}
     return HttpResponse(template.render(context, request))
 
 
