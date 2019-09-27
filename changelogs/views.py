bc20a9cf7a0cecc9b13d9d7bfb89afa179913975
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-09-27 13:34:24 +0500

    Remove error test

--- changelogs/views.py
+++ changelogs/views.py
@@ -8,7 +8,6 @@ from .serializers import ProjectSerializer, VersionSerializer
 
 
 def index(request):
-    123/0
     template = loader.get_template("changelogs/index.html")
     context = {}
     return HttpResponse(template.render(context, request))
