f1d8687beacb3fea960c0a4ffb99ecb83b1260ff
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-09-27 11:12:21 +0500

    Try to fix 500 errors

--- changelogger/settings.py
+++ changelogger/settings.py
@@ -94,7 +94,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
