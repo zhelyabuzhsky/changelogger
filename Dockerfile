3d31ba70a657ba4eeed9f19c04df7af448457830
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-09-27 12:07:48 +0500

    Removed dockerfile and heroku.yml

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.7-slim
-ENV PYTHONUNBUFFERED 1
-ENV DJANGO_SETTINGS_MODULE changelogger.settings
-RUN mkdir /app
-WORKDIR /app
-COPY requirements.txt ./
-RUN pip install -r requirements.txt
-COPY . .
--- changelogger/settings.py
+++ changelogger/settings.py
@@ -95,7 +95,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
