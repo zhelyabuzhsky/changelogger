f3311c803ede893e1211050a5d434596860129e9
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2020-04-15 14:15:35 +0500

    Added fetching of gitlab projects

--- changelogs/management/commands/fetch.py
+++ changelogs/management/commands/fetch.py
@@ -1,76 +1,17 @@
-import os
-from datetime import datetime
-from typing import List, Dict
-
-import requests
 from django.core.management.base import BaseCommand
 
-from ...models import Project, Version
-
-
-def _fetch_github_project(project: Project) -> None:
-    def _run_github_grahql_query(query: str) -> Dict:
-        headers = {"Authorization": f"Bearer {os.environ['GITHUB_TOKEN']}"}
-        request = requests.post(
-            "https://api.github.com/graphql", json={"query": query}, headers=headers
-        )
-        if request.status_code == 200:
-            return request.json()
-        else:
-            raise Exception(
-                "Query failed to run by returning code of {}. {}".format(
-                    request.status_code, query
-                )
-            )
-
-    query = """
-    {
-      repository(owner: "%s", name: "%s") {
-        releases(last: 10) {
-          edges {
-            node {
-              tagName
-              publishedAt
-              description
-            }
-          }
-        }
-      }
-    }
-    """ % (
-        project.repository_owner,
-        project.repository_name,
-    )
-
-    response = _run_github_grahql_query(query)
-
-    releases: List[Dict] = response["data"]["repository"]["releases"]["edges"]
-
-    for release in releases:
-        if not Version.objects.filter(
-            project=project, title=release["node"]["tagName"]
-        ).exists():
-            Version.objects.create(
-                title=release["node"]["tagName"],
-                date_time=datetime.strptime(
-                    release["node"]["publishedAt"], "%Y-%m-%dT%H:%M:%SZ"
-                ),
-                project=project,
-                body=release["node"]["description"],
-            )
+from changelogs.models import Project
+from changelogs.services import fetch_github_project, fetch_gitlab_project
 
 
 class Command(BaseCommand):
     help = "Fetches all projects changelogs"
 
     def handle(self, *args, **options):
-        github_projects = (
-            project
-            for project in Project.objects.all()
-            if project.is_github_project is True
-        )
-
-        for project in github_projects:
-            _fetch_github_project(project)
+        for project in Project.objects.all():
+            if project.is_github_project is True:
+                fetch_github_project(project)
+            else:
+                fetch_gitlab_project(project)
 
         self.stdout.write(self.style.SUCCESS("Successfully fetched changelogs"))
--- changelogs/services.py
+++ changelogs/services.py
@@ -1,8 +1,15 @@
+import json
+import os
+from datetime import datetime
+from typing import Dict, List
+from urllib.parse import urlparse
+
+import requests
 from django.conf import settings
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-from .models import Version
+from .models import Project, Version
 
 
 def send_email_notifications(version: Version) -> None:
@@ -17,3 +24,95 @@ def send_email_notifications(version: Version) -> None:
         )
         sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
         sg.send(message)
+
+
+def _run_graphql_query(query: str, url: str, token: str) -> dict:
+    headers = {"Authorization": f"Bearer {token}"}
+    request = requests.post(url, json={"query": query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception(
+            "Query failed to run by returning code of {}. {}".format(
+                request.status_code, query
+            )
+        )
+
+
+def fetch_github_project(project: Project) -> None:
+    query = """
+    {
+      repository(owner: "%s", name: "%s") {
+        releases(last: 10) {
+          edges {
+            node {
+              tagName
+              publishedAt
+              description
+            }
+          }
+        }
+      }
+    }
+    """ % (
+        project.repository_owner,
+        project.repository_name,
+    )
+
+    response = _run_graphql_query(
+        query, "https://api.github.com/graphql", os.environ["GITHUB_TOKEN"]
+    )
+
+    releases: List[Dict] = response["data"]["repository"]["releases"]["edges"]
+
+    for release in releases:
+        if not Version.objects.filter(
+            project=project, title=release["node"]["tagName"]
+        ).exists():
+            Version.objects.create(
+                title=release["node"]["tagName"],
+                date_time=datetime.strptime(
+                    release["node"]["publishedAt"], "%Y-%m-%dT%H:%M:%SZ"
+                ),
+                project=project,
+                body=release["node"]["description"],
+            )
+
+
+def _get_gitlab_project_id(project: Project) -> int:
+    query = """
+        query {
+            project(fullPath: "%s") {
+                id
+            }
+        }
+    """ % (
+        urlparse(project.url).path[1:-1]
+    )
+    response = _run_graphql_query(
+        query,
+        f"https://{urlparse(project.url).netloc}/api/graphql",
+        project.owner.gitlab_token,
+    )
+    return int(response["data"]["project"]["id"].split("/")[-1])
+
+
+def _get_gitlab_project_releases(project: Project) -> List[Dict]:
+    r = requests.get(
+        f"https://{urlparse(project.url).netloc}/api/v4/projects/{_get_gitlab_project_id(project)}/releases",
+        headers={"Private-Token": project.owner.gitlab_token},
+    )
+    data = json.loads(r.content.decode("utf8"))
+    return data
+
+
+def fetch_gitlab_project(project: Project) -> None:
+    for release in _get_gitlab_project_releases(project):
+        print(release["name"], release["description"], release["released_at"])
+        if not Version.objects.filter(project=project, title=release["name"]).exists():
+            Version.objects.create(
+                title=release["name"],
+                date_time=datetime.fromisoformat(release["released_at"]),
+                project=project,
+                body=release["description"],
+            )
